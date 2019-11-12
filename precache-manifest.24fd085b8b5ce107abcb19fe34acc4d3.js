e0a128e271908b5468291ba3d162b47fabca9e92
tphan <takeonme>
2019-11-13 00:52:01 +0700

    Updates

---
+++ precache-manifest.24fd085b8b5ce107abcb19fe34acc4d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea0487ad9126a5f51224aa437783b201",
+    "url": "/rpsgame/index.html"
+  },
+  {
+    "revision": "fb998e0b26c0ad0221ee",
+    "url": "/rpsgame/static/css/main.90ed0244.chunk.css"
+  },
+  {
+    "revision": "1dbb65d6170b8bdebcab",
+    "url": "/rpsgame/static/js/2.0833c061.chunk.js"
+  },
+  {
+    "revision": "fb998e0b26c0ad0221ee",
+    "url": "/rpsgame/static/js/main.8e0e2e8d.chunk.js"
+  },
+  {
+    "revision": "6dab9f3e712ba731e527",
+    "url": "/rpsgame/static/js/runtime-main.a687775b.js"
+  }
+]);
